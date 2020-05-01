3b6ee494be9792f1864c177be9bacccecc1e832a
Alex Scarlett <alex.scarlett91@gmail.com>
2020-05-01 04:09:05 -0400

    Prepare for deployment with build folder created and entry point (server.js) modified for if in production

---
+++ client/build/precache-manifest.acdff4f14788acfd70376f544fc3feec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa7eb1a0b0bf739876ff02eade6d52b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb3d0f4ede12a3ce9a02",
+    "url": "/static/css/main.a6368c4b.chunk.css"
+  },
+  {
+    "revision": "1999f2e13e76ad44b252",
+    "url": "/static/js/2.a91c904f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a91c904f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb3d0f4ede12a3ce9a02",
+    "url": "/static/js/main.793b9d19.chunk.js"
+  },
+  {
+    "revision": "cd50a7d19c7673d39246",
+    "url": "/static/js/runtime-main.4cada699.js"
+  }
+]);
